2ce660a933f6a46619fc4aafd3444e41e542429e
pfost-bit <ezq9qu@virginia.edu>
2024-08-22 19:25:37 -0400

    Update Euler_1.R

--- Euler_1.R
+++ Euler_1.R
@@ -1,7 +1,7 @@
 #Euler problem number 1, find the sum of all the multiples of 3 and 5 below 1000
 
 
-numbers <- 1:1000
+max_value <- 1000
 
 #output list
 final_list <- numeric()
@@ -9,7 +9,7 @@ final_list <- numeric()
 
 # iterate through the numbers 1-1000, check if they are a multiple of 3 or 5 and add
 # them to the output list
-for (y in numbers){
+for (y in 1:max_value-1){
   if(y %% 3 == 0){
     final_list <- append(final_list, y)
   } else if(y %% 5 == 0){
