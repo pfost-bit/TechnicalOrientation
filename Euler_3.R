c5c38f04bab3cf37bdd6b131c2ad7a212ead99fd
pfost-bit <ezq9qu@virginia.edu>
2024-08-22 21:44:36 -0400

    Add files via upload

---
+++ Euler_3.R
@@ -0,0 +1,38 @@
+#What is the largest prime factor of the number 600851475143? 
+
+#a method to check if a number is prime
+
+is_prime2 <- function(n){
+# factor and its pair converge at the sqrt of the number
+  value <- floor(sqrt(n))+1
+#check to see if it is a factor by comparing checking if divisible by anything
+  for(i in 2:value){
+    if(n %% i == 0){
+      return(FALSE)
+    }
+  }
+  return(TRUE)
+}
+factors <- numeric() 
+
+number <- 600851475143
+max_value <- floor(sqrt(number))+1
+
+#similar to above, a factor converges with its "pair" at its square root,
+# now we check to see if its "pair" is a prime  
+
+for(i in 2:max_value){
+  if (is_prime2(i)){
+    if(number %% i == 0){
+      pair <- number/i
+      factors <- append(factors, i)
+      if(is_prime2(pair)){
+        factors <- append(factors, pair)
+      }
+    }
+  }
+    
+}
+
+
+max(factors)
